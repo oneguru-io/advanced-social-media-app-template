f34a8c9f2ca5ceaf7f8dc69973ad0e92d25b0cd4
unknown <carljacobdiazcruz@gmail.com>
2022-11-20 22:40:28 +0800

    Added: initial template configuration

---
+++ bin/cli.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+const {execSync} = require('child_process');
+
+const runCommand = command => {
+  try {
+    execSync(`${command}`, {stdio: 'inherit'});
+  } catch(e) {    
+    console.error(`Failed to execute ${command}`, e);
+    return false;
+  }
+  return true;
+}
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/oneguru-io/advanced-ecommerce-app-template.git ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if(!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+const installedDeps = runCommand(installDepsCommand);
+if(!installedDeps) process.exit(-1);
+
+console.log('Congratulations! You are ready. Follow the following commands to start');
